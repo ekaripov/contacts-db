a9fd3b47083aa167e18110a8bf71c8b065da5041
Evgeniy <ekaripov@gmail.com>
2021-01-04 23:15:23 +0300

    Generated project with spring.io

---
+++ src/main/java/ru/ekaripov/contactsdb/ContactsDbApplication.java
@@ -0,0 +1,13 @@
+package ru.ekaripov.contactsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ekaripov/contactsdb/ContactsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ekaripov.contactsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
