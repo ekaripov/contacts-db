98245211fbd1ad0d29b930011860cc7a5b94ab22
Dmitry Shishkin <shishkindima@gmail.com>
2021-01-22 18:58:21 +0300

    added History

---
+++ src/main/java/ru/ekaripov/contactsdb/model/History.java
@@ -0,0 +1,44 @@
+package ru.ekaripov.contactsdb.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.ekaripov.contactsdb.entities.HistoryType;
+import ru.ekaripov.contactsdb.entities.Person;
+import ru.ekaripov.contactsdb.entities.User;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(nullable = false)
+    private Date historydate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "history_type_id")
+    private HistoryType historyType;
+}
