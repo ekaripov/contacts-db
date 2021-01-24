18057e04486423006c34ed16ecc80488fb38c4c1
Dmitry Shishkin <shishkindima@gmail.com>
2021-01-24 16:58:49 +0300

    added methods to HistoryService interface

--- src/main/java/ru/ekaripov/contactsdb/service/interf/HistoryService.java
+++ src/main/java/ru/ekaripov/contactsdb/service/interf/HistoryService.java
@@ -1,4 +1,11 @@
 package ru.ekaripov.contactsdb.service.interf;
 
+import ru.ekaripov.contactsdb.model.History;
+
+import java.util.List;
+
 public interface HistoryService {
+    History save(History history);
+
+    List<History> getAllHistoryRecords();
 }
