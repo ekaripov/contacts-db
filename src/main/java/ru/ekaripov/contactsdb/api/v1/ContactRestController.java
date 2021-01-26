c720cd17f7eb6d39c681ecfc08dfe6241c16d53b
Igor <savchigor@gmail.com>
2021-01-26 14:44:06 +0300

    CD-39 added Controllers for Contact and User

---
+++ src/main/java/ru/ekaripov/contactsdb/api/v1/ContactRestController.java
@@ -0,0 +1,26 @@
+package ru.ekaripov.contactsdb.api.v1;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.ekaripov.contactsdb.model.Contact;
+import ru.ekaripov.contactsdb.service.interf.ContactService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/contact")
+@AllArgsConstructor
+public class ContactRestController {
+
+    private final ContactService contactService;
+
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Contact>> getAllContacts() {
+        List<Contact> allContacts = contactService.getAllContacts();
+        return ResponseEntity.ok(allContacts);
+    }
+}
---
+++ src/main/java/ru/ekaripov/contactsdb/api/v1/UserRestController.java
@@ -0,0 +1,26 @@
+package ru.ekaripov.contactsdb.api.v1;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.ekaripov.contactsdb.model.User;
+import ru.ekaripov.contactsdb.service.interf.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@AllArgsConstructor
+public class UserRestController {
+
+    private final UserService userService;
+
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = userService.getAllUsers();
+        return ResponseEntity.ok(allUsers);
+    }
+}
