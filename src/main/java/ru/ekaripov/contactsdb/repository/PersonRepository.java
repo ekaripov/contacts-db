025e0498473c1f7c4778bacd7aadae5423d6cd4b
Evgeniy <ekaripov@gmail.com>
2021-02-18 18:18:35 +0300

    Added repo

---
+++ src/main/java/ru/ekaripov/contactsdb/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package ru.ekaripov.contactsdb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.ekaripov.contactsdb.model.Person;
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
