bf606ede90e663593a3c36d8dd3e31d3f001bc47
Evgeniy <ekaripov@gmail.com>
2021-02-27 19:05:03 +0300

    Added service

--- src/main/java/ru/ekaripov/contactsdb/repository/EventRepository.java
+++ src/main/java/ru/ekaripov/contactsdb/repository/EventRepository.java
@@ -1,9 +1,20 @@
 package ru.ekaripov.contactsdb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.ekaripov.contactsdb.model.Event;
+import ru.ekaripov.contactsdb.model.Person;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByPerson(Person person);
+    @Query(value = "SELECT * FROM events WHERE event_date BETWEEN " +
+            "(CURRENT_DATE - interval '1' day) AND" +
+            "(CURRENT_DATE + interval ':days' day)",
+            nativeQuery = true)
+    List<Event> findUpcomingEventsByDays(@Param("days") int days);
 }
--- src/main/java/ru/ekaripov/contactsdb/service/impl/EventServiceImpl.java
+++ src/main/java/ru/ekaripov/contactsdb/service/impl/EventServiceImpl.java
@@ -2,25 +2,66 @@ package ru.ekaripov.contactsdb.service.impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.ekaripov.contactsdb.exceptions.DatabaseEntryNotFoundException;
+import ru.ekaripov.contactsdb.exceptions.IdNotDefinedException;
 import ru.ekaripov.contactsdb.model.Event;
+import ru.ekaripov.contactsdb.model.Person;
 import ru.ekaripov.contactsdb.repository.EventRepository;
 import ru.ekaripov.contactsdb.service.interf.EventService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class EventServiceImpl implements EventService {
 
-    private final EventRepository eventRepository;
+    private final EventRepository repository;
 
     @Override
-    public Event save(Event event) {
-        return eventRepository.save(event);
+    @Transactional
+    public Event addEvent(Event event) {
+        return repository.save(event);
+    }
+
+    @Override
+    @Transactional
+    public Event updateEvent(Event event) {
+        if (event.getId() == null) throw new IdNotDefinedException();
+        Event editEvent = repository.findById(event.getId()).orElseThrow(DatabaseEntryNotFoundException::new);
+        editEvent.setEventDate(event.getEventDate());
+        editEvent.setEventType(event.getEventType());
+        editEvent.setDescription(event.getDescription());
+        editEvent.setPerson(event.getPerson());
+        editEvent.setUser(event.getUser());
+        return repository.save(editEvent);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEventById(Long id) {
+        repository.findById(id).orElseThrow(DatabaseEntryNotFoundException::new);
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Event> findEventById(Long id) {
+        return repository.findById(id);
     }
 
     @Override
     public List<Event> getAllEvents() {
-        return eventRepository.findAll();
+        return repository.findAll();
+    }
+
+    @Override
+    public List<Event> findEventsByPerson(Person person) {
+        return repository.findByPerson(person);
+    }
+
+    @Override
+    public List<Event> findUpcomingEventsByDays(int days) {
+        return repository.findUpcomingEventsByDays(days);
     }
 }
