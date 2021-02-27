18b40ae31c6ae56050a2ccf5dee9a67488e91ea3
Evgeniy <ekaripov@gmail.com>
2021-02-27 19:17:41 +0300

    Added controller besides find functions

--- src/main/java/ru/ekaripov/contactsdb/api/v1/EventRestController.java
+++ src/main/java/ru/ekaripov/contactsdb/api/v1/EventRestController.java
@@ -2,9 +2,9 @@ package ru.ekaripov.contactsdb.api.v1;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.ekaripov.contactsdb.exceptions.DatabaseEntryNotFoundException;
+import ru.ekaripov.contactsdb.exceptions.IdNotDefinedException;
 import ru.ekaripov.contactsdb.model.Event;
 import ru.ekaripov.contactsdb.model.converter.impl.EventDtoConverter;
 import ru.ekaripov.contactsdb.model.dto.EventDto;
@@ -17,13 +17,47 @@ import java.util.List;
 @AllArgsConstructor
 public class EventRestController {
 
-    private final EventService eventService;
-    private final EventDtoConverter eventDtoConverter;
+    private final EventService service;
+    private final EventDtoConverter converter;
 
     @GetMapping("/getAll")
     public ResponseEntity<List<EventDto>> getAllEvent() {
-        List<Event> allEvents = eventService.getAllEvents();
-        List<EventDto> eventDtoList = eventDtoConverter.convertToDto(allEvents);
-        return ResponseEntity.ok(eventDtoList);
+        return ResponseEntity.ok(converter.convertToDto(service.getAllEvents()));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventDto> findEventById(@PathVariable Long id){
+        return service.findEventById(id)
+                .map(event -> ResponseEntity.ok(converter.convertToDto(event)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDto> addEvent(@RequestBody Event event){
+        event.setId(null);
+        return ResponseEntity.ok(converter.convertToDto(service.addEvent(event)));
+    }
+
+    @PutMapping
+    public  ResponseEntity<EventDto> updateEvent(@RequestBody Event event){
+        try{
+            return ResponseEntity.ok(converter.convertToDto(service.updateEvent(event)));
+        } catch (DatabaseEntryNotFoundException e){
+            return ResponseEntity.notFound().build();
+        } catch (IdNotDefinedException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable Long id){
+        try {
+            service.deleteEventById(id);
+            return ResponseEntity.ok().build();
+        } catch (DatabaseEntryNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
